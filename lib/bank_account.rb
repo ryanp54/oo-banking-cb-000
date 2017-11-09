0fb1775afb99327566a286d862fe647c427393d8
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 23:31:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit(amount)
+    self.balance += amount.to_f
+  end
+  def display_balance
+    "Balance: #{self.balance}"
+  end
+  def valid?
+    self.balance > 0 && self.status == "open"
+  end
+  def close_account
+    self.status = "closed"
+    self.balance = 0
+  end
 end
